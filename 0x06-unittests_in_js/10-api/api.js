1b21a2f2b9b0a60682e7ea079150e6cccbb3f660
abdelghafour99 <abdelghafour83@live.fr>
2024-12-29 17:05:11 +0000

    commit 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 7865;
 
-app.get('/', (_, res) => {
+app.use(express.json());
+
+app.get('/', (_req, res) => {
   res.send('Welcome to the payment system');
 });
 
